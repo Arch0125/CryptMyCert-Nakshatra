42125f23cffb9018262b1a65b6b08dccee846016
Arch0125 <dasarchisman25@gmail.com>
2022-02-11 19:18:38 +0530

    contracts : Add Certificate upload-verification contract

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,5 +1,4 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.4.21 <0.7.0;
+pragma solidity ^0.5.0;
 
 contract Migrations {
   address public owner;
@@ -15,5 +14,10 @@ contract Migrations {
 
   function setCompleted(uint completed) public restricted {
     last_completed_migration = completed;
+  }
+
+  function upgrade(address new_address) public restricted {
+    Migrations upgraded = Migrations(new_address);
+    upgraded.setCompleted(last_completed_migration);
   }
 }
