c8c5fc7800ccc8dc7a1902b40882f7eafcbbe83f
Arch0125 <dasarchisman25@gmail.com>
2022-02-11 20:13:08 +0530

    App.js : Add routes to components

--- client/src/App.js
+++ client/src/App.js
@@ -1,70 +1,41 @@
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
-import getWeb3 from "./getWeb3";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 
+import FileCertificatorPage from './components/FileCertificatorPage/FileCertificatorPage'
+import NavBar from './components/NavBar/NavBar'
+import FaqPage from './components/FaqPage/FaqPage'
+import AboutPage from './components/AboutPage/AboutPage'
+import Intro from "./components/Intro/Intro";
+import Payment from "./components/Payment/Payment"
+
+import AuthenticityContract from "./contracts/Authenticity.json";
+
+import getWeb3 from "./utils/getWeb3";
+import CryptoJS from "crypto-js";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css"
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
 
-  componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
-    } catch (error) {
-      // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
-      console.error(error);
-    }
-  };
-
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
 
   render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
-    }
+
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+      <NavBar />
+      <Router>
+      <Route exact path="/" component={FileCertificatorPage} />
+      <Route path="/faq" component={FaqPage} />
+      <Route path="/about" component={AboutPage} />
+      <Route path="/intro" component={Intro}/>
+      <Route path="/pay" component={Payment}/>
+
+      </Router>
+      <footer>
+        <p className={"footerText"}>© 2022 CryptMyCert</p>
+    </footer>
       </div>
     );
   }
