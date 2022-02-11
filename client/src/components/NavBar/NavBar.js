08004bb2fc038da8a119610111612c0e761f409a
Arch0125 <dasarchisman25@gmail.com>
2022-02-11 21:14:23 +0530

    components : Add NavBar component

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react";
+
+import {
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  Collapse
+} from "shards-react";
+
+// style
+import "./NavBar.css";
+
+import iconimg from "../../assets/iconz.png"
+import logoWeb from "../../assets/logo-web.png"
+
+class NavBar extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dropdownOpen: false,
+      collapseOpen: false
+    };
+  }
+
+  toggleDropdown() {
+  this.setState({
+      ...this.state,
+      ...{
+        dropdownOpen: !this.state.dropdownOpen
+      }
+    });
+  }
+
+  toggleNavbar() {
+    this.setState({
+      ...this.state,
+      ...{
+        collapseOpen: !this.state.collapseOpen
+      }
+    });
+  }
+
+
+
+  render() {
+
+    return (
+      <Navbar id={"appNavBar"} type="dark" expand="md">
+          <NavbarBrand href="/intro">
+            <b>CryptMyCert</b>
+          </NavbarBrand>
+          <NavbarToggler onClick={() => this.toggleNavbar()} />
+
+          <Collapse open={this.state.collapseOpen} navbar>
+              <Nav navbar className="ml-auto">
+              <NavItem>
+              <a class="purpletext" href="/about"><button class="button-34" type="button" >Verification Portal</button></a>
+              </NavItem>
+            </Nav>
+          </Collapse>
+      </Navbar>
+    )
+  }
+}
+
+export default NavBar
